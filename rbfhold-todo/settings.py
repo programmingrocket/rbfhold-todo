17014ef87d654dcd179373d898a89f1b18e390d7
rbfhold <59418278+rbfhold@users.noreply.github.com>
2020-05-02 08:38:27 -0400

    Initialize ToDo Django project

--- rbfhold-todo/settings.py
+++ rbfhold-todo/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for rbfhold-toto project.
 
@@ -122,6 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 django_heroku.settings(locals())
+
